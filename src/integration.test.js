883e51ea6c640949f45fa537ca1fc45d4439d13e
Muntasir Biojid <muntasir2165@hotmail.com>
2019-09-16 09:27:49 -0700

    Ends of lecture 75

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,8 +2,24 @@ import { storeFactory } from "../test/testUtils";
 import { guessWord } from "./actions";
 
 describe("guessWord action dispatcher", () => {
+  const secretWord = "party";
+  const unsuccessfulGuess = "train";
   describe("no guessed words", () => {
-    test("updates state correctly for unsuccessful guess", () => {});
+    let store;
+    const initialState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+    test("updates state correctly for unsuccessful guess", () => {
+      store.dispach(guessWord(unsuccessfulGuess));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{ guessedWord: unsuccessfulGuess, letterMatchCount: 3 }]
+      };
+      expect(newState).toEqual(expectedState);
+    });
     test("updates state correctly for successful guess", () => {});
   });
   describe("some guessed words", () => {
