76ee8ad8be1af16f4f75931deeed33ffc18e2c4e
Muntasir Biojid <mbiojid@cisco.com>
2019-09-26 16:02:53 -0700

    Connect App component (and tests)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
+import { connect } from "react-reduxx";
 import "./App.css";
 
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
+import Input from "./Input";
+import { getSecretWord } from "./actions";
 
 function App() {
   return (
     <div className="container">
       <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
+      <Congrats success={this.props.success} />
+      <GuessedWords guessedWords={this.props.guessedWords} />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSecretWord }
+)(App);
